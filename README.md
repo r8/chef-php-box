49a2baf17ad8b48dce29f9de423a2a26aa5ab9f5
Sergey Storchay <r8@r8.com.ua>
2013-07-26 08:26:41 +0300

    Add license information

---
+++ README.md
@@ -0,0 +1,10 @@
+chef-php-box
+============
+
+License
+-------
+
+Copyright (c) 2013 Sergey Storchay, http://r8.com.ua
+
+Licensed under MIT:
+https://raw.github.com/r8/wp2github.py/master/LICENSE.txt
--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,8 @@
 #
 # Copyright 2013, Sergey Storchay
 #
+# Licensed under MIT:
+# https://raw.github.com/r8/wp2github.py/master/LICENSE.txt
 
 default["php-box"]["install_globally"] = true
 default["php-box"]["prefix"] = "/usr/local"
